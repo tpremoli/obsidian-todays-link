c8877d5402e51afdb6aded6b3db1b61c7a3eede8
tpremoli <tomas@premoli.org>
2026-02-19 23:59:35 +0000

    Initial Commit
    Create command to insert today's daily note at cursor
    
    Functionality to parse [[today]] still not functional but skeleton
    implemented

--- src/main.ts
+++ src/main.ts
@@ -1,99 +1,62 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
-import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
+import {App, Editor, MarkdownView, moment, MarkdownFileInfo, Plugin} from 'obsidian';
+import {UrlIntoSelection} from 'core';
+import {DEFAULT_SETTINGS, TodaysLinkSettings, TodaysLinkSettingsTab} from "./settings";
 
-// Remember to rename these classes and interfaces!
+export default class TodaysLinkObsidian extends Plugin {
+	settings: TodaysLinkSettings;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	todayLinkReplaceHandler = (editor: Editor, info: MarkdownView | MarkdownFileInfo) =>
+		ReplaceTodaysLink(editor, info, this.settings);
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new TodaysLinkSettingsTab(this.app, this));
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
+        const dailyNotesPlugin = this.app.internalPlugins.getPluginById("daily-notes");
+		const instance = dailyNotesPlugin.instance;
+		const format = instance.getFormat() ?? "YYYY-MM-DD";
+		this.settings.DailyNoteFileName = format.includes("/") 
+			? format.substring(format.lastIndexOf("/") + 1) 
+			: format;
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
+        if (!dailyNotesPlugin?.enabled) {
+            console.warn("TodaysLink: Daily Notes core plugin is not enabled. Disabling plugin.");
+            return;
+        }
+
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
+			id: 'add-todays-link-at-cursor',
+			name: 'Add link to today\'s daily note at cursor',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+				editor.processLines(
+					(line, lineText) => line === editor.getCursor().line ? lineText : null,
+					(line, lineText, value) => {
+						if (value === null) return;
+						const ch = editor.getCursor().ch;
+						return {
+							from: { line, ch },
+							to: { line, ch },
+							text: `[[${moment().format(this.settings.DailyNoteFileName)}]]`
+						};
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
+				);
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
+		this.app.workspace.on("editor-change", this.todayLinkReplaceHandler);
 	}
 
 	onunload() {
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<MyPluginSettings>);
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<TodaysLinkSettings>);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
